d4ffde688ad44d1fbd0e02cd37d74e838e2cb8ef
victoor.jelle <victoor.jelle@0cf60be0-794f-0410-a8bf-112e0dd0f7dd>
2008-06-14 13:31:19 +0000

    comment

--- trunk/OnslaughtGame/src/onslaught/gui/OnslaughtFrame.java
+++ trunk/OnslaughtGame/src/onslaught/gui/OnslaughtFrame.java
@@ -6,33 +6,37 @@ import java.awt.Dimension;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.swing.JFrame;
-
+/**
+ * The frame is used to hold all the panels and contains the game loop
+ * @author Jelle
+ */
 public class OnslaughtFrame extends JFrame implements WindowListener {
-	private final int FWIDTH = 800;
-	private final int FHEIGHT = 600;
-	private Zone zone;
-	
-	public OnslaughtFrame() {
-		super();
-		initComponents();
-                addWindowListener(this);
-		zone.start();
-		this.setVisible(true);
-	}
-	
-	public void initComponents(){
-		this.setPreferredSize(new Dimension(FWIDTH, FHEIGHT));
-		zone = new Zone();
-		this.getContentPane().add(zone, BorderLayout.NORTH);
-		this.pack();
-	}
-	
-	public static void main(String[] args){
-		new OnslaughtFrame();
-	}
+
+    private final int FWIDTH = 800;
+    private final int FHEIGHT = 600;
+    private Zone zone;
+
+    public OnslaughtFrame() {
+        super();
+        initComponents();
+        addWindowListener(this);
+        zone.start();
+        this.setVisible(true);
+    }
+
+    public void initComponents() {
+        this.setPreferredSize(new Dimension(FWIDTH, FHEIGHT));
+        zone = new Zone();
+        this.getContentPane().add(zone, BorderLayout.NORTH);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        new OnslaughtFrame();
+    }
 
     public void windowOpened(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void windowClosing(WindowEvent e) {
@@ -41,22 +45,22 @@ public class OnslaughtFrame extends JFrame implements WindowListener {
     }
 
     public void windowClosed(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void windowIconified(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void windowDeiconified(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void windowActivated(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public void windowDeactivated(WindowEvent e) {
-        //throw new UnsupportedOperationException("Not supported yet.");
+    //throw new UnsupportedOperationException("Not supported yet.");
     }
 }
