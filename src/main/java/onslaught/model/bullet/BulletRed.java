2ab4fab4d5603492bcd87b833b14129da445a57f
dries.demeurisse <dries.demeurisse@0cf60be0-794f-0410-a8bf-112e0dd0f7dd>
2012-04-11 19:05:00 +0000

    rocket update

--- src/main/java/onslaught/model/bullet/BulletRed.java
+++ src/main/java/onslaught/model/bullet/BulletRed.java
@@ -4,9 +4,13 @@
  */
 package onslaught.model.bullet;
 
-import java.awt.geom.AffineTransform;
 import onslaught.game.GameOperations;
+import onslaught.game.GameProperties;
+import onslaught.game.OnslaughtGame;
+import onslaught.model.Entity;
 import onslaught.model.enemy.Enemy;
+import onslaught.util.MathUtils;
+import onslaught.util.TimingUtility;
 
 /**
  *
@@ -15,33 +19,67 @@ import onslaught.model.enemy.Enemy;
 public class BulletRed extends Bullet {
 
     private final float MAX_ROTATION_SPEED = 65;
-    private final static int START_ANGLE = -45;
+    private final static int START_ANGLE = -35;
     private final static float SPEED = 400f;
     private static final String URL = "images/bullet-red.png";
-    private AffineTransform affineTransform = new AffineTransform();
-    private double oldShootAngle = Math.toRadians(45);
 
     public BulletRed(double startX, double startY, Enemy target, GameOperations gameOps, int damage) {
         super(URL, startX, startY, SPEED, gameOps, target, damage);
         rotationAngle -= START_ANGLE;
-        startAngle = START_ANGLE;
-        calcAngleToTarget(target, this, 1000000000L, 100000);
-        setDamage(100);
+        calcVelocities(target, TimingUtility.getOneSecond() / GameProperties.WANTED_FPS);
     }
 
     @Override
-    public void update(long elapsedTime) {
-        super.update(elapsedTime);
+    public void update(long currentTime) {
+        if (target != null) {
+            super.update(currentTime);
+            if (this.x > OnslaughtGame.SCREEN_WIDTH || this.x < 0 || this.y > OnslaughtGame.SCREEN_HEIGHT || this.y < 0) {
+                this.kill();
+            }
+        }
+    }
+
+    @Override
+    protected void calcVelocities(Entity target, long passedTime) {
+        double speedStep = TimingUtility.calcTimeFactor(passedTime) * speed;
+
+        double xDist = target.getX() - getX();
+        double yDist = target.getY() - getY();
+        double targetAngle = Math.atan2(yDist, xDist);
+
+        dx = Math.cos(targetAngle) * speedStep;
+        dy = Math.sin(targetAngle) * speedStep;
+
+        rotationAngle = targetAngle * (180 / MathUtils.getPi());
+        rotationAngle -= START_ANGLE;
     }
 
     @Override
     protected void targetDiedEvent() {
         for (Enemy _nmy : gameOps.getEnemies()) {
             if (_nmy.isAlive()) {
-                target = _nmy;
-                break;
+                checkTarget(_nmy);
             }
         }
     }
-
+//        rotationAngle -= START_ANGLE;
+//        startAngle = START_ANGLE;
+//        calcAngleToTarget(target, this, 1000000000L, 100000);
+//        setDamage(100);
+//    }
+//
+//    @Override
+//    public void update(long elapsedTime) {
+//        super.update(elapsedTime);
+//    }
+//
+//    @Override
+//    protected void targetDiedEvent() {
+//        for (Enemy _nmy : gameOps.getEnemies()) {
+//            if (_nmy.isAlive()) {
+//                target = _nmy;
+//                break;
+//            }
+//        }
+//    }
 }
